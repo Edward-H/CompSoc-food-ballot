0fd0a0501fbbe55d80bb2965e3a7e652226d7d4a
Edward Hart <edward.dan.hart@gmail.com>
2017-11-11 08:51:08 +0000

    Initial commit.

---
+++ vote-count.py
@@ -0,0 +1,24 @@
+import json
+from pprint import pprint
+
+with open('sanitised-results.json') as data_file:
+    data = json.load(data_file)
+
+# We apply the Positional Voting system to find the winners: the ones with the
+# lowest rank sum win.
+for section in [u'pizza', u'sides', u'non-alcoholic drinks', u'alcoholic drinks',
+                u'cookies', u'non-cookie food', u'pizza/unhealthy food supplier']:
+    section_prefs = [ballot[section] for ballot in data]
+    section_totals = {}
+    total_votes = 0;
+    for i in range(len(section_prefs)):
+        prefs = section_prefs[i]
+        for option in prefs:
+            votes = sum([(option in prefs and prefs[option])
+                         or len(prefs)])
+            total_votes += len(prefs)
+            if option in section_totals:
+                section_totals[option] += votes
+            else:
+                section_totals[option] = votes + total_votes
+    pprint(sorted(section_totals.items(), key = lambda x: x[1]))
